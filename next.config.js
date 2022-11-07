2f40ca39f4f709dd1a69b72505f9f0cdae25b684
Basic455 <noobcreator47@gmail.com>
2022-11-07 14:17:26 +0530

    deploy

--- next.config.js
+++ next.config.js
@@ -3,10 +3,7 @@ const { loadDefaultErrorComponents } = require('next/dist/server/load-components
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  images: {
-    loader: "akamai",
-    path: "",
-  },
+
 }
 
 module.exports = nextConfig
